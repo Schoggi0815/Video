f2e6262c67e8bd9521fdc81db764658d6e941eda
Matteo Bossi <matteo@encodo.ch>
2022-04-01 11:31:11 +0200

    youtube embed
    thanks to Github-Copilot :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // youtube embed
+  const videoId = 'Slwai2Jhy_A';
+  const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>3D-Print</h1>
+        <div className="video-container">
+          <iframe
+            title="youtube video"
+            width="560"
+            height="315"
+            src={url}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
       </header>
     </div>
   );
